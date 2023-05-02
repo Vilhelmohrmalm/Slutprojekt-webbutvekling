2973c2fda87577078ef6ccb3d64aabe69776e01e
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2023-05-02 09:25:51 +0200

    hje

--- js/Boka.js
+++ js/Boka.js
@@ -1,4 +1,29 @@
 function salong(){
- document.getElementsByClassName(gömd_salong).style.visibility = visible
- 
+    var salong = document.getElementById("salonger");
+    if(salong.style.display == "block"){   
+    salong.style.display = "none"
+    }
+    else{
+    salong.style.display = "block"
+    }
+}
+
+function behandling(){
+    var behandling = document.getElementById("behandling");
+    if(behandling.style.display == "block"){   
+    behandling.style.display = "none"
+    }
+    else{
+    behandling.style.display = "block"
+    }
+}
+
+function tid(){
+    var tid = document.getElementById("tid");
+    if(tid.style.display == "block"){   
+    tid.style.display = "none"
+    }
+    else{
+    tid.style.display = "block"
+    }
 }
