aa21b1655f6736f8289214b8ed0096c3cc404a6a
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2023-05-12 12:14:54 +0200

    heh

---
+++ js/gemensam.js
@@ -0,0 +1,9 @@
+function hamburgare(){
+    var meny = document.getElementById("hamburgarmeny");
+    if(meny.style.display == "block"){   
+    meny.style.display = "none"
+    }
+    else{
+    meny.style.display = "block"
+    }
+}
